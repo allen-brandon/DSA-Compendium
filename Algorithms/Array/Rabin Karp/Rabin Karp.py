f19e70f2c7cc769200d8ba704d58ee0c8c322e53
Brandon Allen <me@brandon-allen.com>
2024-02-17 00:05:10 -0500

    Updated hash collision testing

---
+++ Algorithms/Array/Rabin Karp/Rabin Karp.py
@@ -0,0 +1,50 @@
+#### Hack Protection ####
+# Heavily inspired by Alex Wice's RabinKarp class implementation
+class PrefixHash:
+    #Input a string encoded as an array (e.g. numbers bounded from 0-26)
+    #Initialization is O(n)
+    def __init__(self, arr):
+        self.mod = mod = 344555666677777
+        self.base = base = 37
+        self.inv = inv = pow(base, mod-2, mod)
+        self.pref = pref = [0]
+        self.invpref = invpref = [1]
+        pwr, invpwr = 1, 1
+        hsh = 0
+        for x in arr:
+            hsh = (hsh+x*pwr)%mod
+            pwr = (pwr*base)%mod
+            invpwr = (invpwr*inv)%mod
+            pref.append(hsh)
+            invpref.append(invpwr)
+    #Obtain the hash of a substring [l, ... r] in O(1)
+    #Inclusive of l and r
+    def query(self, l, r):
+        return (self.pref[r+1]-self.pref[l])*self.invpref[l]%self.mod
+    
+### Notes ###
+# 
+
+#### No Hack Protection ####
+# # Heavily inspired by Alex Wice's RabinKarp class implementation
+# class PrefixHash:
+#     #Input a string encoded as an array (e.g. numbers bounded from 0-26)
+#     #Initialization is O(n)
+#     def __init__(self, arr):
+#         self.mod = mod = 344555666677777
+#         self.base = base = 37
+#         self.inv = inv = pow(base, mod-2, mod)
+#         self.pref = pref = [0]
+#         self.invpref = invpref = [1]
+#         pwr, invpwr = 1, 1
+#         hsh = 0
+#         for x in arr:
+#             hsh = (hsh+x*pwr)%mod
+#             pwr = (pwr*base)%mod
+#             invpwr = (invpwr*inv)%mod
+#             pref.append(hsh)
+#             invpref.append(invpwr)
+#     #Obtain the hash of a substring [l, ... r] in O(1)
+#     #Inclusive of l and r
+#     def query(self, l, r):
+#         return (self.pref[r+1]-self.pref[l])*self.invpref[l]%self.mod
